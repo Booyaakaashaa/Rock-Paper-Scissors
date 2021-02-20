c1e33c2ead81a73955377289c1c821cf3a782d83
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-20 13:39:44 +0530

    RPS with computer: Project 2/5

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,9 +1,26 @@
 # Write your code here
+import random
+opt = random.choice(['rock', 'paper', 'scissors'])
 user = input()
 
-if user == 'rock':
-    print("Sorry, but the computer chose paper")
-elif user == 'paper':
-    print("Sorry, but the computer chose scissors")
-elif user == 'scissors':
-    print("Sorry, but the computer chose rock")
+if opt == 'rock':
+    if user == 'rock':
+        print('There is a draw (rock)')
+    elif user == 'scissors':
+        print('Sorry, but the computer chose rock')
+    else:
+        print('Well done. The computer chose rock and failed')
+elif opt == 'scissors':
+    if user == 'scissors':
+        print('There is a draw (scissors)')
+    elif user == 'paper':
+        print('Sorry, but the computer chose scissors')
+    else:
+        print('Well done. The computer chose scissors and failed')
+elif opt == 'paper':
+    if user == 'paper':
+        print('There is a draw (paper)')
+    elif user == 'rock':
+        print('Sorry, but the computer chose paper')
+    else:
+        print('Well done. The computer chose paper and failed')
