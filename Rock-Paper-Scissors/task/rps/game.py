8f0ff0f1a7f67b09c5df4aa22bde8a700f27a1ae
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-20 13:49:11 +0530

    Invalid inputs managed: Project 3/5

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,26 +1,35 @@
 # Write your code here
 import random
-opt = random.choice(['rock', 'paper', 'scissors'])
-user = input()
-
-if opt == 'rock':
-    if user == 'rock':
-        print('There is a draw (rock)')
-    elif user == 'scissors':
-        print('Sorry, but the computer chose rock')
-    else:
-        print('Well done. The computer chose rock and failed')
-elif opt == 'scissors':
-    if user == 'scissors':
-        print('There is a draw (scissors)')
-    elif user == 'paper':
-        print('Sorry, but the computer chose scissors')
-    else:
-        print('Well done. The computer chose scissors and failed')
-elif opt == 'paper':
-    if user == 'paper':
-        print('There is a draw (paper)')
-    elif user == 'rock':
-        print('Sorry, but the computer chose paper')
-    else:
-        print('Well done. The computer chose paper and failed')
+while 1:
+    user = input()
+    if user == "!exit":
+        print("Bye!")
+        break
+    opt = random.choice(['rock', 'paper', 'scissors'])
+    if opt == 'rock':
+        if user == 'rock':
+            print('There is a draw (rock)')
+        elif user == 'scissors':
+            print('Sorry, but the computer chose rock')
+        elif user == "paper":
+            print('Well done. The computer chose rock and failed')
+        else:
+            print('Invalid input')
+    elif opt == 'scissors':
+        if user == 'scissors':
+            print('There is a draw (scissors)')
+        elif user == 'paper':
+            print('Sorry, but the computer chose scissors')
+        elif user == "rock":
+            print('Well done. The computer chose scissors and failed')
+        else:
+            print('Invalid input')
+    elif opt == 'paper':
+        if user == 'paper':
+            print('There is a draw (paper)')
+        elif user == 'rock':
+            print('Sorry, but the computer chose paper')
+        elif user == 'scissors':
+            print('Well done. The computer chose paper and failed')
+        else:
+            print('Invalid input')
