17468f1b0e9bdfc26ea9a369f9cf11447b155769
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-02-20 13:26:52 +0530

    Basic Input tests: Project 1/5

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,2 +1,9 @@
 # Write your code here
+user = input()
 
+if user == 'rock':
+    print("Sorry, but the computer chose paper")
+elif user == 'paper':
+    print("Sorry, but the computer chose scissors")
+elif user == 'scissors':
+    print("Sorry, but the computer chose rock")
